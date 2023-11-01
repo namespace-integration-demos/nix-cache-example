7b9980d9f2503dafd9dea0d98ac727bc6ca7c5de
Hugo T R <htr@namespacelabs.com>
2023-11-01 11:24:24 +0100

    rmeoved shell.nix; cleaned up flake.nix

--- flake.nix
+++ flake.nix
@@ -13,32 +13,5 @@
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      devShell = pkgs.mkShell {
-        buildInputs = with pkgs;
-          [
-            clang-tools # For clang-format.
-            go_1_21
-            gopls
-            go-outline
-            go-tools
-            upx
-            yarn
-            pre-commit
-
-            goreleaser
-
-            eksctl
-            postgresql
-
-            git
-            nodejs
-            crane
-            kubectl
-            awscli2
-            jq
-            aws-iam-authenticator
-            google-cloud-sdk
-          ];
-      };
     });
 }
